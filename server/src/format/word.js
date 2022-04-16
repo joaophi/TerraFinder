8a36c1257fa9027fb124a37f7e49892fa1d5e04d
João Pedro Hickmann <joaophickmann@outlook.com>
2022-04-16 15:19:00 -0300

    Move server para repositorio próprio

--- server/src/format/word.js
+++
@@ -1,8 +0,0 @@
-import { isDenomTerraNative, readDenom } from "terra-utils"
-
-export const getWord = (word) => {
-    const content = isDenomTerraNative(word) ? readDenom(word) : word
-    return content
-}
-
-export default getWord
